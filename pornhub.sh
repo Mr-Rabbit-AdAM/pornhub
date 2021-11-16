ef191815814c4b3649f5e94223fca4c65fc2cec3
Ad-AM <87221134+Mr-Rabbit-AdAM@users.noreply.github.com>
2021-11-17 03:42:40 +0630

    Update pornhub.sh

--- pornhub.sh
+++ pornhub.sh
@@ -31,8 +31,8 @@ for i in $(seq $(cat res.txt | wc -l ))
 do 
 echo ""
 sleep 2 
-echo -e "\e[1;32m [#] Title :: "$(cat title.txt | sed -n "${i}p" )
-echo -e "\e[1;32m [#] Link :: https://www.pornhub.com/view_video"$(cat res.txt | sed -n "${i}p" )
+echo -e "\e[1;33m [#] Title :: "$(cat title.txt | sed -n "${i}p" )
+echo -e "\e[1;32m [#] Link :: [4;36m https://www.pornhub.com/view_video"$(cat res.txt | sed -n "${i}p" )
 echo ""
 done 
 rm -rf res.txt 
